bbf6b541095a66f2679e773b95192285e1168cf0
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-25 13:41:11 +0000

    [Add]ユーザー機能設定、FontAwsomeインストールしてね

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
          
   def get_profile_image(width, height)#ユーザーアイコン設定#
       unless profile_image.attached?
-        file_path = Rails.root.join('app/assets/image/sample-author1.jpg')
-        profile_image.attach(io: File.open(file.path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+        file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+        profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
       end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
