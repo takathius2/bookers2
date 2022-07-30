3d4d46f80217fbeffe54c59434cc7fa0c7a132f2
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-07-30 13:51:49 +0000

    [Add]Userのshow/Bookのindex

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'books/book.id' => 'books#create'
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:new, :show, :edit, :index, :create]
+  resources :books, only: [:new, :show, :edit, :index, :create, :destroy]
   resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220730125308_add_user_introduction_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserIntroductionToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :user_introduction, :text
+  end
+end
