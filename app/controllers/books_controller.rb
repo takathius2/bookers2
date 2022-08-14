bd2e7020ae37ee29fe726e5fa8d6e72c21c2f346
takahius2 <106050338+takathius2@users.noreply.github.com>
2022-08-14 15:33:49 +0000

    [Add]バリデーションして

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,14 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
+    @user_image = @user.profile_image
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+   if @book.save
+     flash[:notice] = "You have created book successfully."
+     redirect_to book_path(@book.id)
+   else
+     render :index
+   end
   end
 
   def index
@@ -28,8 +33,13 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    @users = @user.profile_image
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
